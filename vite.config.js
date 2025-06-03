9159e21f0b091559b975b46af3f0e2124c039a01
aboelazm-gif <3aseerelsabbar@gmail.com>
2025-06-03 05:20:56 +0300

    Fixed path issues with assets

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/assets/**'], // Include all files in the assets folder
 })
